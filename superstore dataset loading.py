53e63ff884ab6561e852fc58725a0f2ee4af7116
JamesKitenye <kitennykiluva@gmail.com>
2025-07-15 00:30:10 +0300

    update
    j-k.

---
+++ superstore dataset loading.py
@@ -0,0 +1,12 @@
+import kagglehub
+import os
+import pandas as pd
+
+dataset_root_path = kagglehub.dataset_download("mohamed38/superstoredataset")
+print(f"Dataset is located at: {dataset_root_path}")
+
+# List files in the downloaded dataset directory
+print("Files in the dataset directory:")
+for root, dirs, files in os.walk(dataset_root_path):
+    for file in files:
+        print(os.path.join(root, file))
